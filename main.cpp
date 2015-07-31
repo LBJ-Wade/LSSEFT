5d64b46e4c2a5fcfd095203837eabba734002d7c
ds283 <D.Seery@sussex.ac.uk>
2015-07-31 11:18:41 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by David Seery on 31/07/15.
+// Copyright (c) 2015 University of Sussex. All rights reserved.
+//
+
+#include <iostream>
+
+#include "boost/program_options.hpp"
+
+int main(int argc, char* argv[])
+  {
+
+  }
