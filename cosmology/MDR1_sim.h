26ba46cc15bdcd8ae419ba1639425090c60a5503
ds283 <D.Seery@sussex.ac.uk>
2017-03-17 11:51:01 +0000

    Add cosmological parameters for BigMDPL model

--- cosmology/MDR1_sim.h
+++ cosmology/MDR1_sim.h
@@ -39,7 +39,7 @@ namespace MDR1
     constexpr double            omega_cc = 0.73;
     constexpr double            omega_m  = 0.27;
     constexpr double            h        = 0.70;
-    constexpr double            f_baryon = 0.0469/0.27;
+    constexpr double            f_baryon = 0.0469/omega_m;
     constexpr Mpc_units::energy H0       = 100 * h * Mpc_units::Kilometre / (Mpc_units::Second * Mpc_units::Mpc);
     constexpr Mpc_units::energy T_CMB    = 2.7255 * Mpc_units::Kelvin;
     constexpr double            Neff     = 3.046;   // Standard Model value
@@ -50,10 +50,38 @@ namespace MDR1
     constexpr Mpc_units::energy kpiv = 0.05 / Mpc_units::Mpc;
     constexpr double sigma8          = 0.8200;
     
-    // CMB-related redshifts (extrated from CAMB)
+    // CMB-related redshifts (extracted from CAMB)
     constexpr double z_star = 1088.02;
     constexpr double z_drag = 1060.39;
     constexpr double z_eq   = 3161.49;
+    
+  }
+
+
+// Parameters for the Big MultiDark Planck simulation from https://www.cosmosim.org/cms/simulations/bigmdpl/
+namespace BigMDPL
+  {
+    
+    const     std::string       name     = "Matches BigMDPL simulation https://www.cosmosim.org/cms/simulations/bigmdpl/";
+    
+    constexpr double            omega_cc = 0.692885;
+    constexpr double            omega_m  = 0.307115;
+    constexpr double            h        = 0.6777;
+    constexpr double            f_baryon = 0.048206/omega_m;
+    constexpr Mpc_units::energy H0       = 100 * h * Mpc_units::Kilometre / (Mpc_units::Second * Mpc_units::Mpc);
+    constexpr Mpc_units::energy T_CMB    = 2.7255 * Mpc_units::Kelvin;
+    constexpr double            Neff     = 3.046;   // Standard Model value
+    
+    // fluctuation two-point function
+    constexpr double Acurv           = 1.9716e-9;
+    constexpr double ns              = 0.96;
+    constexpr Mpc_units::energy kpiv = 0.05 / Mpc_units::Mpc;
+    constexpr double sigma8          = 0.8228;
+    
+    // CMB-related redshifts (extracted from CAMB)
+    constexpr double z_star = 1088.75;
+    constexpr double z_drag = 1063.06;
+    constexpr double z_eq   = 3596.22;
     
   }
 
